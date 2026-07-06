fd6fb0c72b4e2dbd31fa506926c7427ccf1af977
Arman Hossain Hridoy <ah.hridoy997@gmail.com>
2026-07-06 17:09:32 +0600

    chore: initialize React + Vite project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className="text-5xl font-primary text-red-500 text-center font-bold p-20">Welcome to Vite + React</h2>
+    </>
+  )
+}
+
+export default App
