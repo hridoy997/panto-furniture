59fd2b5398f85e7083748af38dc2d1c6536f7db2
Arman Hossain Hridoy <ah.hridoy997@gmail.com>
2026-07-12 22:47:54 +0600

    Add to Cart funtionality

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import { router } from './Routes/Route';
 import './index.css'
-// import 'sweetalert2/src/sweetalert2.scss'
 import 'sweetalert2/dist/sweetalert2.min.css'
 
 
